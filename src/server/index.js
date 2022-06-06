2c8a5c1cadf590fff38d0b645a32fe1f4de8766c
cjlee <chunglee3224@gmail.com>
2022-06-06 17:48:24 +0900

    fix javascript problem

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -10,7 +13,8 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    // res.sendFile(path.resolve('dist/index.html'))
+    res.sendFile(path.resolve('index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
--- webpack.dev.js
+++ webpack.dev.js
@@ -8,12 +8,21 @@ module.exports = {
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
